499a2bb03ac0befce26d0c935fa9f7c507571ae2
NotSanti <santiluna03@outlook.com>
2023-02-12 17:58:39 -0500

    api setup

---
+++ server/app.js
@@ -0,0 +1,36 @@
+/**
+ * This is the main entry point of the application.
+ * Api is being listened in bin/www
+ */
+const express = require("express");
+const routes = require("./routes/index");
+// const imageRoutes = require("./routes/images");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const app = express();
+const database = require("./db/database");
+
+// let connection = async () => {
+//   await database.connect();
+// };
+// connection();
+
+// Serving files from client/build
+app.use(express.static("../client/dist"));
+
+//allow json
+app.use(express.json());
+// All routes will be handled by routes/index.js
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+
+app.use("/api", routes);
+// app.use("/api", imageRoutes);
+
+// Default 404 route
+app.use((_, res) => {
+  res.sendStatus(404);
+});
+
+module.exports = app;
---
+++ server/routes/index.js
@@ -0,0 +1,30 @@
+/**
+ * Main Router to access other routes
+ */
+
+const express = require("express");
+const router = express.Router();
+// const database = require("../db/database");
+
+router.get("/", (_, res) => {
+  res.status(200).json({ message: "Hello World from the server API" });
+});
+
+// //post request to add to db
+// router.post("/data", async (req, res) => {
+//   //store json {“username”: “dan”, “comment”: “hello!” } in db
+//   if (!req.body.username || !req.body.comment) {
+//     res.sendStatus(404);
+//     return;
+//   }
+//   await database.populate(req.body.username, req.body.comment);
+//   res.sendStatus(200);
+// });
+
+// //get request to fetch all items in db
+// router.get("/data", async (req, res) => {
+//   let data = await database.getAllEntries();
+//   res.status(200).json(data);
+// });
+
+module.exports = router;
