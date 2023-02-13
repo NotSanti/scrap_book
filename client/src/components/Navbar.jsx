55b030639861899f4bbfc5f907b0c06361529f70
NotSanti <santiluna03@outlook.com>
2023-02-13 00:15:22 -0500

    started nav bar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">Random</Link>
+      <Link to="/all">All</Link>
+      <Link to="/search">Search</Link>
+    </nav>
+  );
+};
+export default Navbar;
---
+++ client/src/pages/all.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const All = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>ALL </h1>
+    </>
+  );
+};
+
+export default All;
